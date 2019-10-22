38815b6ca2bbdf16018f276b597c6fb29865e567
Cole Smith <cole.f.smith1@gmail.com>
2019-10-22 16:40:29 -0400

    Added AU19 Tidyverse code

---
+++ AU19 - R Tidyverse/Code/TidyOutline.R
@@ -0,0 +1,149 @@
+# R BASICS
+
+# this is a comment
+
+# variable assignments
+min_var = 0
+max_var <- 500
+
+# run code by putting your cursor on the line
+# then hit run (or Ctr/Cmd + Enter)
+
+# Functions are called with a name followed by a set of parenthesis
+# Function arguments are inside the parenthesis
+# this prints ‘0’
+print(min_var)
+
+# this prints a string of characters announcing yourself to the world
+print("Hello World")
+
+
+
+
+# Import tidyverse
+library(???)
+
+# Importing the Data
+# Data URL: https://bdaa-workshops.s3.us-east-2.amazonaws.com/AB_NYC_2019.csv
+???
+
+
+# ------
+# ggplot
+ggplot(data = AB_NYC) # creates a blank plot
+
+# one variable plots
+# density of price
+ggplot(data = AB_NYC) + geom_density(mapping = aes(x=???))
+# density of price, with x axis limited to $2,000
+ggplot(data = AB_NYC) + 
+  geom_density(mapping = aes(x = price)) + 
+  xlim(0,???)
+# density of price, with x axis limited to $500, and colored by neighboorhood_group
+# can try 'fill' instead of 'color'
+ggplot(data = AB_NYC) +
+  geom_density(mapping = aes(x = price, color = ???)) + 
+  xlim(0,500)
+
+# bar plot to see count of rentals by room_type
+ggplot(data = AB_NYC) + 
+  geom_bar(mapping = aes(x = ???))
+# bar plot to see count of rentals by neighbourhood_group, with room_type as a fill
+ggplot(data = AB_NYC) + 
+  geom_bar(mapping = aes(x = ???, fill = ???)) 
+
+# simplifying things
+base = ggplot(data = AB_NYC)
+
+
+# 2 variable plots
+# boxplot to see price by neighbourhood_group
+base + 
+  geom_boxplot(mapping = aes(x = ???, y = ???))
+
+# combining the 2 above plots:
+# boxplot to see price by neighbourhood_group and room_type (via color or fill)
+base + 
+  geom_boxplot(mapping = aes(x = neighbourhood_group, y = price, fill = ???)) + 
+  ylim(0, 500)
+
+# 'mapping' the neighbourhoods 
+# you can set aesthetic values in the ggplot function ("globally") or in each layer
+ggplot(data = AB_NYC, mapping = aes(x = longitude, y = latitude)) + 
+  geom_point(aes(color = neighbourhood_group)) + 
+  coord_quickmap()
+
+# scatterplot of number_of_reviews and price
+ggplot(data = AB_NYC) +
+  geom_point(mapping = aes(x = price, y = number_of_reviews, color = room_type))
+
+
+
+
+# ------------
+# Adding dplyr into the mix
+# filtering data
+manhattan_data = 
+  AB_NYC %>% 
+  filter(neighbourhood_group == "???", room_type == "???")
+
+# scatterplot of number_of_reviews and price
+# looking only at Private Rooms in Manhattan
+# add availability_365 as a color
+ggplot(data = ???) +
+  geom_point(mapping = aes(x = price, y = number_of_reviews, color = ???))
+
+# adding a column with mutate()
+# let's calculate the length of the listing's title (name) 
+# on the manhattan_data
+manhattan_data = 
+  ??? %>% 
+  mutate(title_length = str_length(???))
+
+# generating summary statistics with group_by() and summarise()
+# group_by() the calculated_host_listings_count 
+# to see differences in listings based on how many other listings the host has
+# things to calculate for each calculated_host_listings_count:
+#   - number of listings
+#   - mean price 
+#   - mean number of reviews 
+#   - mean availability_365 
+summary = 
+  AB_NYC %>% 
+  group_by(???) %>% 
+  summarise(
+    num_listings = n(),
+    mean_price = mean(???),
+    mean_num_reviews = mean(???),
+    mean_availability_365 = mean(???)
+  )
+
+# calculate the number of hosts at each calculated_host_listings_count
+# and remove rows where there are 2 or less hosts that host that many listings
+# for the summary dataframe
+summary = 
+  ??? %>% 
+  mutate(num_hosts = num_listings / calculated_host_listings_count) %>% 
+  filter(??? > ???)
+
+# let's see if hosts that host more properties have 
+# more/less availability on avg. than hosts that only have a few listings
+# scatter plot (geom_smooth()) using summary dataframe
+# calculated_host_listings_count compared to mean_availability_365
+# size as num_listings
+# addding a "smoother" as an additional layer
+final_graph = 
+  ggplot(summary, aes(x = calculated_host_listings_count, y = mean_availability_365)) + 
+  geom_point(aes(size = num_listings)) +
+  geom_smooth()
+final_graph
+
+# finishing touches
+# xlab, ylab, ggtitle
+final_graph = final_graph +
+  xlab("Number of Host's Listings") +
+  ylab("Mean Days Available in the Year") +
+  ggtitle("Listing Availablility by Number of Host's Listings")
+final_graph
+
+
---
+++ AU19 - R Tidyverse/README.md
@@ -0,0 +1,3 @@
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/25988130/60761473-70487700-a017-11e9-9bb4-6e26da55c569.png" width="600"/>
+</p>
